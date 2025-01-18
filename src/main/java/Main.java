b2ba2e9977d7ddb5c64eed858c1495e35754519e
Hugo <hugorosa123@gmail.com>
2025-01-18 19:33:33 +0900

    Working PlacesAPI calls

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,28 +4,44 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 public class Main {
     public static void main(String[] args) {
-
+        fetchFromAPI(args[0]);
     }
 
-    public void fetchFromAPI(){
+    public static void fetchFromAPI(String LocationRequest) {
+        Dotenv dotenv = Dotenv.load(); // Automatically reads the `.env` file
+        String apiKey = dotenv.get("PLACES_API_KEY");
+
+        if (apiKey == null || apiKey.isEmpty()) {
+            throw new IllegalStateException("API key is not set in the .env file");
+        }
+
         System.out.println("Fetching from Places API");
         String url = "https://places.googleapis.com/v1/places:searchText";
+        int maxResultCount = 10;
+
+        JsonObject payload = new JsonObject();
+
+        payload.addProperty("maxResultCount", maxResultCount);
+        payload.addProperty("textQuery", LocationRequest);
+
+        String jsonPayload = new Gson().toJson(payload);
 
         try (HttpClient client = HttpClient.newHttpClient()) {
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .GET()
-                    .build();
-
-            HttpRequest request = HttpRequest.newBuilder().GET()
                     .header("maxResultCount","10")
                     .header("X-Goog-Api-Key", apiKey)
-                    .header("X-Goog-FieldMask", "places.primaryType,places.priceLevel,")
+                    .header("X-Goog-FieldMask", "places.primaryType,places.priceLevel,places.rating,places.userRatingCount")
+                    .uri(URI.create(url))
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonPayload))
+                    .build();
 
-
-                    .uri(URI.create(url)).build();
 
 
             HttpResponse<String> response = client.send(
@@ -34,6 +50,7 @@ public class Main {
 
             String jsonData = response.body();
             System.out.println(jsonData);
+            System.out.println("Response status code: " + response.statusCode());
 
         }
         catch (IOException | InterruptedException e) {
